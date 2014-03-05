c75e59317afce552cffba57a03dbefdc8eeb4427
RockwellC09 <RockwellC09@gmail.com>
2014-03-05 15:10:45 -0600

    Added Week 1 Project Files and Folders

---
+++ Week1/SimplEmail/src/com/RockwellChristopher/simplemail/MainActivity.java
@@ -0,0 +1,35 @@
+/*
+ *	Author:		Christopher Rockwell
+ * 
+ * 	Project: 	SimplEmail
+ * 
+ * 	Package: 	com.RockwellChristopher.simplemail
+ * 
+ * 	File: 		MainActivity.java
+ * 
+ *	Purpose:
+ * 
+ */
+
+
+package com.RockwellChristopher.simplemail;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainActivity.
+ */
+public class MainActivity extends Activity {
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
