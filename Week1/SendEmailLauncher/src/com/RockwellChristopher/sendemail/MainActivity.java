d55ccb6165d882cbe3c98ca53878870c76fae890
RockwellC09 <RockwellC09@gmail.com>
2014-03-06 15:29:28 -0600

    Format Code for Final Submission

--- Week1/SendEmailLauncher/src/com/RockwellChristopher/sendemail/MainActivity.java
+++ Week1/SendEmailLauncher/src/com/RockwellChristopher/sendemail/MainActivity.java
@@ -7,9 +7,11 @@
  * 
  * 	File: 		MainActivity.java
  * 
- *	Purpose:	Launcher for my SimplEmail application
+ *	Purpose:	This activity launches the SimplEmail application and sends it a default email address
  * 
  */
+
+
 
 package com.RockwellChristopher.sendemail;
 
@@ -23,38 +25,39 @@ import android.widget.ImageButton;
 public class MainActivity extends Activity {
 	ImageButton imgButton;
 	Button btn;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		// initialize elements
 		imgButton = (ImageButton) findViewById(R.id.img_btn);
 		btn = (Button) findViewById(R.id.launch_btn);
-		
+
 		imgButton.setOnClickListener(new View.OnClickListener() {
-			
-            public void onClick(View v) {
-                // Perform action on click
-            	
-            	Intent i = new Intent(Intent.ACTION_SEND);
-            	i.setType("text/plain"); 
-            	i.putExtra(Intent.EXTRA_EMAIL, "mdf3test@gmail.com");
-            	startActivity(Intent.createChooser(i, "Send email via"));
-            }
+
+			public void onClick(View v) {
+
+				// create email intent chooser
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i.setType("text/plain"); 
+				i.putExtra(Intent.EXTRA_EMAIL, "mdf3test@gmail.com");
+				startActivity(Intent.createChooser(i, "Send email via"));
+			}
 		});
-		
+
 		btn.setOnClickListener(new View.OnClickListener() {
-			
-            public void onClick(View v) {
-                // Perform action on click
-            	
-            	Intent i = new Intent(Intent.ACTION_SEND);
-            	i.setType("text/plain"); 
-            	i.putExtra(Intent.EXTRA_EMAIL, "mdf3test@gmail.com");
-            	startActivity(Intent.createChooser(i, "Send email via"));
-            }
-        });
+
+			public void onClick(View v) {
+
+				// create email intent chooser
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i.setType("text/plain"); 
+				i.putExtra(Intent.EXTRA_EMAIL, "mdf3test@gmail.com");
+				startActivity(Intent.createChooser(i, "Send email via"));
+			}
+		});
 	}
 
 }
--- Week1/SimplEmail/src/com/RockwellChristopher/simplemail/MainActivity.java
+++ Week1/SimplEmail/src/com/RockwellChristopher/simplemail/MainActivity.java
@@ -7,7 +7,8 @@
  * 
  * 	File: 		MainActivity.java
  * 
- *	Purpose:
+ *	Purpose:	This activity sends an email to and from the email addresses specified by the users and allows the users to add
+ * 				smileys/emojis to the message as well
  * 
  */
 
@@ -35,6 +36,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -61,7 +63,7 @@ public class MainActivity extends Activity {
 	ImageButton smiley4;
 	ImageButton smiley5;
 	ImageButton smiley6;
-	
+
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -70,7 +72,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		context = this;
-		
+
 		// initialize view elements
 		from = (TextView) findViewById(R.id.from);
 		to = (TextView) findViewById(R.id.to);
@@ -78,23 +80,23 @@ public class MainActivity extends Activity {
 		msg = (TextView) findViewById(R.id.msg);
 		send = (Button) findViewById(R.id.send);
 		from = (TextView) findViewById(R.id.from);
-		
+
 		fromEdit = (EditText) findViewById(R.id.from_edit);
 		toEdit = (EditText) findViewById(R.id.to_edit);
 		sbjEdit = (EditText) findViewById(R.id.sbj_edit);
 		msgEdit = (EditText) findViewById(R.id.msg_edit);
-		
+
 		smiley1 = (ImageButton) findViewById(R.id.smiley1);
 		smiley2 = (ImageButton) findViewById(R.id.smiley2);
 		smiley3 = (ImageButton) findViewById(R.id.smiley3);
 		smiley4 = (ImageButton) findViewById(R.id.smiley4);
 		smiley5 = (ImageButton) findViewById(R.id.smiley5);
 		smiley6 = (ImageButton) findViewById(R.id.smiley6);
-		
+
 		// set custom fonts
 		Typeface customFont = Typeface.createFromAsset(getAssets(),"DroidSerif-Bold.ttf");
 		Typeface customFont2 = Typeface.createFromAsset(getAssets(), "Cambo-Regular.ttf");
-		
+
 		from.setTypeface(customFont);
 		to.setTypeface(customFont);
 		sbj.setTypeface(customFont);
@@ -104,35 +106,36 @@ public class MainActivity extends Activity {
 		toEdit.setTypeface(customFont2);
 		sbjEdit.setTypeface(customFont2);
 		msgEdit.setTypeface(customFont2);
-		
-		// Get intent, action and MIME type
-	    Intent intent = getIntent();
-	    String action = intent.getAction();
-	    String type = intent.getType();
 
-	    if (Intent.ACTION_SEND.equals(action) && type != null) {
-	        if ("text/plain".equals(type)) {
-	        	// Handle text being sent
-	            handleSendText(intent);
-	        }
-	    }
+		// Get intent, action and MIME type
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+
+		if (Intent.ACTION_SEND.equals(action) && type != null) {
+			if ("text/plain".equals(type)) {
+				// Handle text being sent
+				handleSendText(intent);
+			}
+		}
 
 		send.setOnClickListener(new View.OnClickListener() {
-			
-            public void onClick(View v) {
-                // Perform action on click
-            	sendMail(toEdit.getText().toString(), sbjEdit.getText().toString(), msgEdit.getText().toString());
-            }
-        });
-		
+
+			public void onClick(View v) {
+				// send the what in the toEdit EditText as an email
+				sendMail(toEdit.getText().toString(), sbjEdit.getText().toString(), msgEdit.getText().toString());
+			}
+		});
+
+		// set click listeners for smileys/emojis
 		smiley1.setOnClickListener(new View.OnClickListener() {
-			
-            public void onClick(View v) {
-            	// add emoji/smiley to email 
-            	msgEdit.append("😁");
-            }
-        });
-		
+
+			public void onClick(View v) {
+				// add emoji/smiley to email 
+				msgEdit.append("😁");
+			}
+		});
+
 		smiley2.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
@@ -164,7 +167,7 @@ public class MainActivity extends Activity {
 				msgEdit.append("😖");
 			}
 		});
-		
+
 		smiley6.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
@@ -172,110 +175,118 @@ public class MainActivity extends Activity {
 				msgEdit.append("😡");
 			}
 		});
-   
+
 	}
-	
+
+	// get the text sent from my handler
 	void handleSendText(Intent intent) {
-	    String sharedText = intent.getStringExtra(Intent.EXTRA_EMAIL);
-	    if (sharedText != null) {
-	        // Update UI to reflect text being shared
-	    	fromEdit.setText(sharedText);
-	    }
+		String sharedText = intent.getStringExtra(Intent.EXTRA_EMAIL);
+		if (sharedText != null) {
+			// Update UI to reflect text being shared
+			fromEdit.setText(sharedText);
+		}
 	}
-	
+
+	// create the email message
 	private Message createMessage(String email, String subject, String messageBody, Session session) throws MessagingException, UnsupportedEncodingException {
-	    Message message = new MimeMessage(session);
-	    message.setFrom(new InternetAddress(fromEdit.getText().toString(), ""));
-	    message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
-	    message.setSubject(subject);
-	    message.setText(messageBody);
-	    return message;
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(fromEdit.getText().toString(), ""));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+		message.setSubject(subject);
+		message.setText(messageBody);
+		return message;
 	}
-	
+
+	// send the email
 	private void sendMail(String email, String subject, String messageBody) {
-	    Session session = createSessionObject();
-	 
-	    try {
-	        Message message = createMessage(email, subject, messageBody, session);
-	        new SendMailTask().execute(message);
-	    } catch (AddressException e) {
-	        e.printStackTrace();
-	    } catch (MessagingException e) {
-	        e.printStackTrace();
-	    } catch (UnsupportedEncodingException e) {
-	        e.printStackTrace();
-	    }
+		Session session = createSessionObject();
+
+		try {
+			Message message = createMessage(email, subject, messageBody, session);
+			new SendMailTask().execute(message);
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private Session createSessionObject() {
-	    Properties properties = new Properties();
-	    properties.put("mail.smtp.auth", "true");
-	    properties.put("mail.smtp.starttls.enable", "true");
-	    properties.put("mail.smtp.host", "smtp.gmail.com");
-	    properties.put("mail.smtp.port", "587");
-	 
-	    return Session.getInstance(properties, new javax.mail.Authenticator() {
-	        protected PasswordAuthentication getPasswordAuthentication() {
-	            return new PasswordAuthentication("mdf3test@gmail.com", "devtest3");
-	        }
-	    });
+		// setup gmail STMP server for sending the email
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+
+		return Session.getInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("mdf3test@gmail.com", "devtest3");
+			}
+		});
 	}
-	
+
+	// Send Mail AsyncTask that tries to send the email and catches the possible errors
 	private class SendMailTask extends AsyncTask<Message, Void, Void> {
-	    private ProgressDialog progressDialog;
-	 
-	    @Override
-	    protected void onPreExecute() {
-	        super.onPreExecute();
-	        progressDialog = ProgressDialog.show(MainActivity.this, "Please wait", "Sending mail", true, false);
-	    }
-	 
-	    @Override
-	    protected void onPostExecute(Void aVoid) {
-	        super.onPostExecute(aVoid);
-	        progressDialog.dismiss();
-	        if (hasError) {
-	        	failMessage();
-	        } else {
-	        	successMessage();
-	        }
-	    }
-	 
-	    @Override
-	    protected Void doInBackground(Message... messages) {
-	        try {
-	            Transport.send(messages[0]);
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	            hasError = true;
-	        }
-	        return null;
-	    }
+		private ProgressDialog progressDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressDialog = ProgressDialog.show(MainActivity.this, "Please wait", "Sending email", true, false);
+		}
+
+		@Override
+		protected void onPostExecute(Void aVoid) {
+			super.onPostExecute(aVoid);
+			progressDialog.dismiss();
+			if (hasError) {
+				failMessage();
+			} else {
+				successMessage();
+			}
+		}
+
+		@Override
+		protected Void doInBackground(Message... messages) {
+			try {
+				Transport.send(messages[0]);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+				hasError = true;
+				Log.e("Error: ", e.getMessage().toString());
+			}
+			return null;
+		}
 	}
-	
+
+	// alert shown when email is successful
 	public void successMessage() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("Email Sent");
-        builder.setMessage("Your email was sent successfully!");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // Dismiss dialog
-            }
-        });
-        builder.show();
+		builder.setMessage("Your email was sent successfully!");
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// Dismiss dialog
+			}
+		});
+		builder.show();
 	}
-	
+
+	// alert shown when sending the email is unsuccessful
 	public void failMessage() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("Error");
-        builder.setMessage("There was an error and your email wasn't sent.");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // Show Toast message
-            	Toast.makeText(context, "Double check your email address(es) to make sure they're correct.", Toast.LENGTH_LONG).show();
-            	hasError = false;
-            }
-        });
-        builder.show();
+		builder.setMessage("There was an error and your email wasn't sent.");
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// Show Toast message
+				Toast.makeText(context, "Double check your email address(es) to make sure they're correct.", Toast.LENGTH_LONG).show();
+				hasError = false;
+			}
+		});
+		builder.show();
 	}
 }
