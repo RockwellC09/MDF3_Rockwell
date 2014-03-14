99c4ac1bab4b523aa6d1d96d3796c90c38bedad3
RockwellC09 <RockwellC09@gmail.com>
2014-03-13 21:16:34 -0500

    Formatted code for final turn in

--- Week2/Statustician/src/com/RockwellChristopher/statustician/BatteryService.java
+++ Week2/Statustician/src/com/RockwellChristopher/statustician/BatteryService.java
@@ -12,7 +12,6 @@
  * 
  */
 
-
 package com.RockwellChristopher.statustician;
 
 import android.app.Notification;
@@ -22,20 +21,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.BatteryManager;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 
 public class BatteryService extends Service {
 
 	static BroadcastReceiver receiver;
-	
+	private static final String PREF_IS_RUNNING = "RUN";
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		// TODO Auto-generated method stub
@@ -46,75 +48,93 @@ public class BatteryService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
-		
+
 		receiver = new BroadcastReceiver() {
 
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				// TODO Auto-generated method stub
+				// check for battery level and show notification if requirements
+				// are met
 				int battery = getBattery();
 				if (battery <= 25) {
 					serviceStopped();
 					stopService();
-				} else if (battery == 50){
+				} else if (battery == 50) {
 					halfCharge();
-				} else if (battery == 100){
+				} else if (battery == 100) {
 					chargeComplete();
 				}
 			}
-			
+
 		};
 		IntentFilter intentFil = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 		this.registerReceiver(receiver, intentFil);
-		
+
 		return super.onStartCommand(intent, flags, startId);
 	}
-	
+
 	public void serviceStopped() {
 		NotificationManager notManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(this)
-			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentTitle("Battery Service Stopped")
-			.setContentText("Stopped due to low battery");
+		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Battery Service Stopped")
+				.setContentText("Stopped due to low battery");
 		Notification notification = notBuilder.build();
 		notManager.notify(01, notification);
 	}
-	
+
 	public void chargeComplete() {
 		NotificationManager notManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(this)
-			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentTitle("Battery Fully Charged!")
-			.setContentText("Unplug your device");
+		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Battery Fully Charged!")
+				.setContentText("Unplug your device");
 		Notification notification = notBuilder.build();
 		notManager.notify(02, notification);
 	}
-	
+
 	public void halfCharge() {
 		NotificationManager notManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(this)
-			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentTitle("Battery at 50 Percent")
-			.setContentText("Consider charging your device soon");
+		NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Battery at 50 Percent")
+				.setContentText("Consider charging your device soon");
 		Notification notification = notBuilder.build();
 		notManager.notify(03, notification);
 	}
-	
+
 	private void stopService() {
 		// TODO Auto-generated method stub
-		this.unregisterReceiver(receiver);
+		// stop BatteryService
+		Intent service = new Intent(getApplicationContext(),
+				BatteryService.class);
+		getApplicationContext().stopService(service);
+		setRunning(false);
+		MainActivity.serviceTV.setText("Service: Not Running");
 	}
 
-	private int getBattery () {
+	// get current battery level
+	private int getBattery() {
 		IntentFilter intentFil = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-		Intent batteryIntent =  this.registerReceiver(null, intentFil);
-		int lev =  batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL,  -1);
+		Intent batteryIntent = this.registerReceiver(null, intentFil);
+		int lev = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 		int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 		int percentage = 0;
 		if (lev > 0 && scale > 0) {
 			percentage = (lev * 100) / scale;
 		}
-		
+
 		return percentage;
+	}
+
+	// set preference to whether this service is running or not
+	private void setRunning(boolean running) {
+		SharedPreferences pref = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor editor = pref.edit();
+
+		editor.putBoolean(PREF_IS_RUNNING, running);
+		editor.apply();
 	}
 }
--- Week2/Statustician/src/com/RockwellChristopher/statustician/MainActivity.java
+++ Week2/Statustician/src/com/RockwellChristopher/statustician/MainActivity.java
@@ -11,7 +11,6 @@
  * 
  */
 
-
 package com.RockwellChristopher.statustician;
 
 import android.app.Activity;
@@ -31,23 +30,23 @@ import android.preference.PreferenceManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
-	
+public class MainActivity extends Activity implements OnClickListener {
+
 	TextView batteryTv;
 	TextView phoneTv;
 	TextView networkTv;
 	TextView batteryTvTitle;
 	TextView phoneTvTitle;
 	TextView networkTvTitle;
-	TextView serviceTV;
+	static TextView serviceTV;
 	Button startBtn;
-	Button stopBtn;
+	static Button stopBtn;
 	Context context;
 	TelephonyManager telManager;
 	PhoneStateListener phoneStateLis;
@@ -60,6 +59,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	BroadcastReceiver batReceiver;
 	IntentFilter intentFil;
 	Intent batteryIntent;
+	int percentage;
 	private static final String PREF_IS_RUNNING = "RUN";
 
 	@Override
@@ -67,7 +67,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		context = this;
-		
+
 		// initialize view elements
 		batteryTv = (TextView) findViewById(R.id.textView1);
 		phoneTv = (TextView) findViewById(R.id.textView2);
@@ -78,9 +78,10 @@ public class MainActivity extends Activity implements OnClickListener{
 		serviceTV = (TextView) findViewById(R.id.service_tv);
 		startBtn = (Button) findViewById(R.id.start);
 		stopBtn = (Button) findViewById(R.id.stop);
-		
+
 		// set custom font
-		Typeface customFont = Typeface.createFromAsset(getAssets(),"SigmarOne.ttf");
+		Typeface customFont = Typeface.createFromAsset(getAssets(),
+				"SigmarOne.ttf");
 		batteryTv.setTypeface(customFont);
 		phoneTv.setTypeface(customFont);
 		networkTv.setTypeface(customFont);
@@ -90,173 +91,210 @@ public class MainActivity extends Activity implements OnClickListener{
 		serviceTV.setTypeface(customFont);
 		startBtn.setTypeface(customFont);
 		stopBtn.setTypeface(customFont);
-		
+
 		// add click listeners to start and stop buttons
 		startBtn.setOnClickListener(this);
 		stopBtn.setOnClickListener(this);
-		
+
 		// check preference to see if service was set to running
 		if (isRunning(context)) {
 			Intent service = new Intent(context, BatteryService.class);
-	        context.startService(service);
+			context.startService(service);
 		} else {
 			Intent service = new Intent(context, BatteryService.class);
-	        context.stopService(service);
+			context.stopService(service);
 		}
-        
-        // check to see if service is running and output to user
-        if(isMyServiceRunning()) { 
-        	// service is run
-        	serviceTV.setText("Service: Running");
-        }else {
-        	// service not running
-        	serviceTV.setText("Service: Not Running");
-        }
-        
-        // get battery status
-        getBatteryStatus();
-        
-        // receiver for change in battery
-        batReceiver = new BroadcastReceiver() {
+
+		// check to see if service is running and output to user
+		if (isMyServiceRunning()) {
+			// service is running
+			serviceTV.setText("Service: Running");
+		} else {
+			// service not running
+			serviceTV.setText("Service: Not Running");
+		}
+
+		// get battery status
+		getBatteryStatus();
+
+		// receiver for change in battery
+		batReceiver = new BroadcastReceiver() {
 
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				// TODO Auto-generated method stub
 				getBatteryStatus();
 			}
-			
+
 		};
-		
+
 		// register receiver
 		intentFil = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-		batteryIntent =  this.registerReceiver(batReceiver, intentFil);
-        
-        // setup telephone manager
-        telManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        
-        phoneStateLis = new PhoneStateListener() {
-        	@Override
-        	public void onCallStateChanged(int state, String incomingNumber) {
-        		
-        		phoneType = telManager.getPhoneType();
-        		// get phone type
-        		if (phoneType == TelephonyManager.PHONE_TYPE_CDMA) {
-        			typeStr = "CDMA";
-        		} else if (phoneType == TelephonyManager.PHONE_TYPE_GSM) {
-        			typeStr = "GSM";
-        		} else if (phoneType == TelephonyManager.PHONE_TYPE_SIP) {
-        			typeStr = "SIP";
-        		}
-        		
-        		// get phone state
-        		if (state == TelephonyManager.CALL_STATE_IDLE) {
-        			stateStr = "Idle";
-        		} else if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
-        			stateStr = "On a Call";
-        		} else if (state == TelephonyManager.CALL_STATE_RINGING) {
-        			stateStr = "Ringing";
-        			numRings++;
-        		}
-        		
-        		if (incomingNumber.length() > 1) {
-        			inNum = incomingNumber;
-        		}
-        		
-        		phoneTv.setText(Html.fromHtml("<font color=\"#545454\"><b>Phone Type: </b></font>" + typeStr + 
-        				"<br /><font color=\"#545454\"><b>Phone State: </b></font>" + stateStr + "<br />" +
-        				"<font color=\"#545454\"><b>Incoming Number: </b></font>" + inNum + "<br />" +
-        				"<font color=\"#545454\"><b>Rang: </b></font>" + numRings + " times"));
-        	}
-        };
-        
-        // add listener
-        telManager.listen(phoneStateLis, PhoneStateListener.LISTEN_CALL_STATE);
+		batteryIntent = this.registerReceiver(batReceiver, intentFil);
+
+		// setup telephone manager
+		telManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+		phoneStateLis = new PhoneStateListener() {
+			@Override
+			public void onCallStateChanged(int state, String incomingNumber) {
+
+				phoneType = telManager.getPhoneType();
+				// get phone type
+				if (phoneType == TelephonyManager.PHONE_TYPE_CDMA) {
+					typeStr = "CDMA";
+				} else if (phoneType == TelephonyManager.PHONE_TYPE_GSM) {
+					typeStr = "GSM";
+				} else if (phoneType == TelephonyManager.PHONE_TYPE_SIP) {
+					typeStr = "SIP";
+				}
+
+				// get phone state
+				if (state == TelephonyManager.CALL_STATE_IDLE) {
+					stateStr = "Idle";
+				} else if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+					stateStr = "On a Call";
+				} else if (state == TelephonyManager.CALL_STATE_RINGING) {
+					stateStr = "Ringing";
+					numRings++;
+				}
+
+				if (incomingNumber.length() > 1) {
+					inNum = incomingNumber;
+				}
+
+				phoneTv.setText(Html
+						.fromHtml("<font color=\"#545454\"><b>Phone Type: </b></font>"
+								+ typeStr
+								+ "<br /><font color=\"#545454\"><b>Phone State: </b></font>"
+								+ stateStr
+								+ "<br />"
+								+ "<font color=\"#545454\"><b>Incoming Number: </b></font>"
+								+ inNum
+								+ "<br />"
+								+ "<font color=\"#545454\"><b>Rang: </b></font>"
+								+ numRings + " times"));
+			}
+		};
+
+		// add listener
+		telManager.listen(phoneStateLis, PhoneStateListener.LISTEN_CALL_STATE);
 	}
 
-	// get batter health string base on the INT given by EXTRA_HEALTH i from the battery manager
+	// get batter health string base on the INT given by EXTRA_HEALTH i from the
+	// battery manager
 	private String getBatteryHealthStr(int code) {
 		String healthStr = "";
 		switch (code) {
-			case 1:
-				healthStr = "Unknown";
-				break;
-			case 2:
-				healthStr = "Good";
-				break;
-			case 3:
-				healthStr = "Overheating";
-				break;
-			case 4:
-				healthStr = "Dead";
-				break;
-			case 5:
-				healthStr = "Over Voltage";
-				break;
-			case 6:
-				healthStr = "Unspecified Failure";
-				break;
-			case 7:
-				healthStr = "Cold";
-				break;
-			
+		case 1:
+			healthStr = "Unknown";
+			break;
+		case 2:
+			healthStr = "Good";
+			break;
+		case 3:
+			healthStr = "Overheating";
+			break;
+		case 4:
+			healthStr = "Dead";
+			break;
+		case 5:
+			healthStr = "Over Voltage";
+			break;
+		case 6:
+			healthStr = "Unspecified Failure";
+			break;
+		case 7:
+			healthStr = "Cold";
+			break;
+
 		}
 
 		return healthStr;
-		
+
 	}
-	
+
 	@Override
 	public void onResume() {
 		// get connection status
-        getConnectionInfo();
+		getConnectionInfo();
 		super.onResume();
 	}
-	
+
 	// get the current battery information and output it to the user
 	private void getBatteryStatus() {
 		// register receiver
 		intentFil = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-		batteryIntent =  this.registerReceiver(batReceiver, intentFil);
-		int lev =  batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL,  -1);
+		batteryIntent = this.registerReceiver(batReceiver, intentFil);
+		int lev = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 		int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-		int healthCode = batteryIntent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
-		
+		int healthCode = batteryIntent.getIntExtra(BatteryManager.EXTRA_HEALTH,
+				-1);
+
 		if (lev > 0 && scale > 0) {
-			int percentage = (lev * 100) / scale;
-			batteryTv.setText(Html.fromHtml("<font color=\"#545454\"><b>Battery Level: </b></font>" + percentage + "% <br />"
-								+ "<font color=\"#545454\"><b>Battery Health: </b></font>" + getBatteryHealthStr(healthCode)));
+			percentage = (lev * 100) / scale;
+			batteryTv
+					.setText(Html
+							.fromHtml("<font color=\"#545454\"><b>Battery Level: </b></font>"
+									+ percentage
+									+ "% <br />"
+									+ "<font color=\"#545454\"><b>Battery Health: </b></font>"
+									+ getBatteryHealthStr(healthCode)));
 		}
 	}
-	
+
 	// check to see if service is running
 	private boolean isMyServiceRunning() {
-	    ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-	        if ("com.RockwellChristopher.statustician.BatteryService".equals(service.service.getClassName())) {
-	            return true;
-	        }
-	    }
-	    return false;
+		ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+		for (RunningServiceInfo service : manager
+				.getRunningServices(Integer.MAX_VALUE)) {
+			if ("com.RockwellChristopher.statustician.BatteryService"
+					.equals(service.service.getClassName())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		if (v == startBtn) {
-			// start BatteryService
-			Intent service = new Intent(context, BatteryService.class);
-	        context.startService(service);
-	        setRunning(true);
-			serviceTV.setText("Service: Running");
-		} else if (v == stopBtn){
-			// stop BatteryService
-			Intent service = new Intent(context, BatteryService.class);
-			context.stopService(service);
-			setRunning(false);
-        	serviceTV.setText("Service: Not Running");
+			if (percentage > 25) {
+				// check to see if service is running and output to user
+				if (isMyServiceRunning()) {
+					// do nothing
+				} else {
+					// service not running
+					// start BatteryService
+					Intent service = new Intent(context, BatteryService.class);
+					context.startService(service);
+					setRunning(true);
+					serviceTV.setText("Service: Running");
+				}
+			} else {
+				Toast.makeText(context,
+						"Service can't be started due to low battery.",
+						Toast.LENGTH_LONG).show();
+			}
+
+		} else if (v == stopBtn) {
+			if (percentage > 25) {
+				// check to see if service is running and output to user
+				if (isMyServiceRunning()) {
+					// stop BatteryService
+					Intent service = new Intent(context, BatteryService.class);
+					context.stopService(service);
+					setRunning(false);
+					serviceTV.setText("Service: Not Running");
+				} else {
+					// do nothing
+				}
+			}
+
 		}
 	}
-	
+
 	// set preference to whether this service is running or not
 	private void setRunning(boolean running) {
 		SharedPreferences pref = PreferenceManager
@@ -273,18 +311,20 @@ public class MainActivity extends Activity implements OnClickListener{
 				.getDefaultSharedPreferences(ctx.getApplicationContext());
 		return pref.getBoolean(PREF_IS_RUNNING, false);
 	}
-	
+
 	private void getConnectionInfo() {
 		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo ni = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 		boolean isMobile = ni.isConnected();
-		
+
 		if (isMobile) {
 			connStr = "On";
 		} else {
 			connStr = "Off";
 		}
-		
-		networkTv.setText(Html.fromHtml("<font color=\"#545454\"><b>Mobile Data: </b></font>" + connStr));
+
+		networkTv.setText(Html
+				.fromHtml("<font color=\"#545454\"><b>Mobile Data: </b></font>"
+						+ connStr));
 	}
 }
